24679731e3d5862d3ab038b9cd7c3b705e2181a2
bfalk8 <bfalk8@gmail.com>
2017-01-23 09:45:06 -0800

    changed out the text fields for shift options for selection boxes

--- src/client/components/EnigmaSettings.jsx
+++ src/client/components/EnigmaSettings.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import style from './EnigmaSettings.css';
 
-let options = [...Array(8).keys()];
+let rotorOptions = [...Array(8).keys()];
+let shiftOptions = [...Array(26).keys()];
 
 class EnigmaSettings extends Component {
   constructor(props) {
@@ -24,7 +25,7 @@ class EnigmaSettings extends Component {
             <p>Left Rotor</p>
             <select name='leftRotor' onChange={this.handleUpdate} 
               value={this.props.leftRotor}>
-              {options.map((x) => (<option value={x+1}>{x+1}</option>))}
+              {rotorOptions.map((x) => (<option value={x+1}>{x+1}</option>))}
             </select>
           </div>
 
@@ -32,7 +33,7 @@ class EnigmaSettings extends Component {
             <p>Middle Rotor</p>
             <select name='middleRotor' onChange={this.handleUpdate} 
               value={this.props.middleRotor}>
-              {options.map((x) => (<option value={x+1}>{x+1}</option>))}
+              {rotorOptions.map((x) => (<option value={x+1}>{x+1}</option>))}
             </select>
           </div>
 
@@ -40,7 +41,7 @@ class EnigmaSettings extends Component {
             <p>Right Rotor</p>
             <select name='rightRotor' onChange={this.handleUpdate} 
               value={this.props.rightRotor}>
-              {options.map((x) => (<option value={x+1}>{x+1}</option>))}
+              {rotorOptions.map((x) => (<option value={x+1}>{x+1}</option>))}
             </select>
           </div>
 
@@ -48,29 +49,26 @@ class EnigmaSettings extends Component {
         <div className={style.row}>
           <div className={style.col}>
             <p>Left Shift</p>
-            <input type="text" name='leftShift' value={this.props.leftShift} 
-              onChange={this.handleUpdate}
-              maxlength={2}
-              size={2}
-            />
+            <select name='leftShift' onChange={this.handleUpdate} 
+              value={this.props.leftShift}>
+              {shiftOptions.map((x) => (<option value={x}>{x}</option>))}
+            </select>
           </div>
 
           <div className={style.col}>
             <p>Middle Shift</p>
-            <input type="text" name='middleShift' value={this.props.middleShift} 
-              onChange={this.handleUpdate}
-              maxlength={2}
-              size={2}
-            />
+            <select name='middleShift' onChange={this.handleUpdate} 
+              value={this.props.middleShift}>
+              {shiftOptions.map((x) => (<option value={x}>{x}</option>))}
+            </select>
           </div>
 
           <div className={style.col}>
             <p>Right Shift</p>
-            <input type="text" name='rightShift' value={this.props.rightShift} 
-              onChange={this.handleUpdate}
-              maxlength={2}
-              size={2}
-            />
+            <select name='rightShift' onChange={this.handleUpdate} 
+              value={this.props.rightShift}>
+              {shiftOptions.map((x) => (<option value={x}>{x}</option>))}
+            </select>
           </div>
         </div>
       </div>
